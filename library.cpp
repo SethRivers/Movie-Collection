7996507898ddb82a0c35f28864029076bb73c24f
Seth Rivers <davmrive@ut.utm.edu>
2024-03-15 23:27:30 -0500

    Worked on print function. Headache slowed progress

--- library.cpp
+++ library.cpp
@@ -8,151 +8,119 @@
  */
 
 #include <iostream>
-#include <iomanip>
 #include <fstream>
-#include <string>
 #include "library.h"
 
 using namespace std;
 
-
-/**
- * 
- *
- * @pre 
- * @post 
- * 
- */
-library::library();
+Library::Library(){
+  Movie * head = nullptr;
+  Movie * tail = nullptr;
+  Movie * midpoint = nullptr;
+  int number_of_movies = 0;
+}
 
 
-/**
- * 
- *
- * @param std::string Title 
- * @param std::string Director_Name 
- * @param int Movie_Runtime 
- * @param std::string Format 
- * @param float Price 
- * @param int Year 
- * @pre 
- * @return void 
- * @post 
- * 
- */
-void library::push_back(std::string Title, std::string Director_Name, int Movie_Runtime, std::string Format, float Price, int Year);
+void Library::push_back(std::string Title, std::string Director_Name, int Movie_Runtime, std::string Format, float Price, int Year){
+Movie * NewMovie = new Movie;
+  NewMovie -> next = nullptr;
+  NewMovie -> Title = New_Title;
+  NewMovie -> Director_Name = New_Director_Name;
+  NewMovie -> Movie_Runtime = New_Movie_Runtime;
+  NewMovie -> Format = New_Format;
+  NewMovie -> Price = New_Price;
+  NewMovie -> Year = New_Year;
+  number_of_movies++;
+
+  if(head == nullptr)
+    {
+      head = NewMovie;
+      midpoint = NewMovie;
+      tail = NewMovie;
+    }
+  else
+    {
+      tail = head;
+
+      //find the last Node (NewMovie) in the list and assign tail to that Node.
+      while(tail->next != NULL)
+	{
+	  tail = tail->next;
+	}
+      tail -> next = NewMovie;
+    }
+}
+
+void Library::push_front(std::string Title, std::string Director_Name, int Movie_Runtime, std::string Format, float Price, int Year){
+
+}
 
 
-/**
- * 
- *
- * @param std::string Title 
- * @param std::string Director_Name 
- * @param int Movie_Runtime 
- * @param std::string Format 
- * @param float Price 
- * @param int Year 
- * @pre 
- * @return void 
- * @post 
- * 
- */
-void library::push_front(std::string Title, std::string Director_Name, int Movie_Runtime, std::string Format, float Price, int Year);
+void Library::load_from_file(std::string filename){
+
+}
+
+void Library::store_to_file(std::stirng filename){
+
+}
 
 
-/**
- * 
- *
- * @param std::string filename 
- * @pre 
- * @return void 
- * @post 
- * 
- */
-void library::load_from_file(std::string filename);
+void Library::insert_sorted(std::string Title, std::string Director_Name, int Movie_Runtime, std::string Format, float Price, int Year){
+
+}
 
 
-/**
- * 
- *
- * @param std::stirng filename 
- * @pre 
- * @return void 
- * @post 
- * 
- */
-void library::store_to_file(std::stirng filename);
+void Library::find_movie(std::string Query_Title){
+  Movie *temp = new Movie;
+  temp = head;
+  if (head == NULL){
+    cout<<"Library is empty! Please enter in some films"<<endl;
+    return;
+  }
+  while (temp != NULL){
+    if(
+}
 
 
-/**
- * 
- *
- * @param std::string Title 
- * @param std::string Director_Name 
- * @param int Movie_Runtime 
- * @param std::string Format 
- * @param float Price 
- * @param int Year 
- * @pre 
- * @return void 
- * @post 
- * 
- */
-void library::insert_sorted(std::string Title, std::string Director_Name, int Movie_Runtime, std::string Format, float Price, int Year);
+string Library::director_search(std::string Director_Name){
+
+}
 
 
-/**
- * 
- *
- * @param std::string Title 
- * @pre 
- * @return string 
- * @post 
- * 
- */
-string library::find_movie(std::string Title);
+void Library::remove(std::string Title){
+
+}
 
 
-/**
- * 
- *
- * @param std::string Director_Name 
- * @pre 
- * @return string 
- * @post 
- * 
- */
-string library::director_search(std::string Director_Name);
+void Library::print(){
+    entry *temp = head;
+    if(head == NULL)
+      {
+	cout << "No Movies present in library! Please select a new file or add some fresh cinematography!" << endl;
+	return;
+      }
+    while(temp != NULL)
+      {
+	cout<< temp->Title <<endl;
+	cout<< temp->Director_Name <<endl;
+	cout<< temp->Format <<endl;
+	cout<< temp->Movie_Runtime <<endl;
+	cout<< temp->Year <<endl;
+	cout<< temp->Price <<endl; 
+	temp = temp->next;
+      }
+}
 
 
-/**
- * 
- *
- * @param std::string Title 
- * @pre 
- * @return void 
- * @post 
- * 
- */
-void library::remove(std::string Title);
-
-
-/**
- * 
- *
- * @pre 
- * @return void 
- * @post 
- * 
- */
-void library::print();
-
-
-/**
- * 
- *
- * @pre 
- * @post 
- * 
- */
-library::~library();
+Library::~Library(){
+  Movie * current = head;
+  while(current != NULL)
+    {
+      Movie * Next_To_Delete = current->next;
+      delete current;
+      current = Next_To_Delete;
+    }
+  head = nullptr;
+  tail = nullptr;
+  number_of_movies = 0;
+}
