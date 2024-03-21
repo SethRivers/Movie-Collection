a8158f8dd351b848356991249c08358bd24dcf64
James Hart <jhart44@ut.utm.edu>
2024-03-21 01:34:44 -0500

    Added void find_move(string Query_Title)

--- library.cpp
+++ library.cpp
@@ -49,6 +49,11 @@ void Library::load_from_file(string filename)
 }
 void Library::print()
 {
+  if(number_of_movies <= 0)
+    {
+      cout << "There are no movies in the list to print! Add some movies to the list!" << endl;
+      return;
+    }
   cout << "List of " << number_of_movies << " items" << endl;
   for(it = Movie_List.begin(); it != Movie_List.end(); it++)
     {
@@ -57,6 +62,11 @@ void Library::print()
   it = Movie_List.begin(); //To avoid possible errors, we should always point to the head of the list.
 }
 void Library::Insert_Sort() {
+  if(number_of_movies <= 0)
+    {
+      cout << "There are no movies in the list to sort! Add some movies to the list!" << endl;
+      return;
+    }
     std::list<Movie>::iterator it2;
     Movie temp;
     
@@ -77,6 +87,11 @@ void Library::Insert_Sort() {
 
 void Library::remove(string Query_Title)
 {
+  if(number_of_movies <= 0)
+    {
+      cout << "There are no movies in the list to delete! Add some movies to the list!" << endl;
+      return;
+    }
   for(it = Movie_List.begin(); it != Movie_List.end(); it++)
     {
       if((it -> Title) == Query_Title)
@@ -88,3 +103,49 @@ void Library::remove(string Query_Title)
     }
   
 }
+
+void Library::push_back(std::string New_Title, std::string New_Director_Name, int New_Movie_Runtime, std::string New_Format, float New_Price, int New_Year)
+{
+  Movie temp;
+  temp.Title = New_Title;
+  temp.Director_Name = New_Director_Name;
+  temp.Movie_Runtime = New_Movie_Runtime;
+  temp.Format = New_Format;
+  temp.Price = New_Price;
+  temp.Year = New_Year;
+  Movie_List.push_back(temp);
+  cout << "Movie added to the back of the list!" << endl;
+}
+
+void Library::push_front(std::string New_Title, std::string New_Director_Name, int New_Movie_Runtime, std::string New_Format, float New_Price, int New_Year)
+{
+  Movie temp;
+  temp.Title = New_Title;
+  temp.Director_Name = New_Director_Name;
+  temp.Movie_Runtime = New_Movie_Runtime;
+  temp.Format = New_Format;
+  temp.Price = New_Price;
+  temp.Year = New_Year;
+  Movie_List.push_front(temp);
+  cout << "Movie added to the front of the list!" << endl;
+}
+
+void Library::find_movie(string Query_Title)
+{
+  if(number_of_movies <= 0)
+    {
+      cout << "There are no movies in the list to find! Add some movies to the list!" << endl;
+      return;
+    }
+  cout << "List of " << Query_Title << " " << endl;
+  for(it = Movie_List.begin(); it != Movie_List.end(); it++)
+    {
+      if(it -> Title == Query_Title)
+	{
+	  cout << "List item: " << it -> Title << " - " << it -> Director_Name << " - " << it -> Format << endl;
+	}
+    }
+  it = Movie_List.begin(); //To avoid possible errors, we should always point to the head of the list.
+
+}
+
